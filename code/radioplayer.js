07ea9533f06730c271794bcc04e609cfde5e5de0
maek95 <mekstrom01@gmail.com>
2023-11-21 14:12:32 +0100

    done, didnt do stretch goals

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,12 +1,19 @@
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
 
-// DOMContentLoaded Event: Ensure that your JavaScript code runs after the DOM has fully loaded. You can achieve this by wrapping your code in the DOMContentLoaded event listener... If I don't do this the channelsEl.appendChild(newChannelContainer); does not work... fetch going too fast for api?
+// DOMContentLoaded Event: Ensure that your JavaScript code runs after the DOM has fully loaded. You can achieve this by wrapping your code in the DOMContentLoaded event listener... 
+// fixed the issue in index.html where we placed <div id="channels"> </div> AFTER the script.js link (should be before).
 document.addEventListener("DOMContentLoaded", function () {
   const channelsEl = document.getElementById("channels");
+  channelsEl.style.minWidth = "600px";
 
   async function getChannels() {
 
-    const response = await fetch("https://api.sr.se/api/v2/channels/?format=json");
+    let response;
+    try {
+       response = await fetch("http://api.sr.se/api/v2/channels?format=json&size=100");
+    } catch (err) {
+      console.log(err); // TypeError: failed to fetch
+    }
 
     const data = await response.json();
     console.log(data); // object of objects..?
@@ -20,28 +27,45 @@ document.addEventListener("DOMContentLoaded", function () {
     data.channels.forEach( (channel) => {
       console.log(channel); // channeltype, color, id, image, imagetemplate, liveaudio, name, scheduleurl, siteurl, tagline, xmltvid...
       const newChannelContainer = document.createElement("div");
-      channelsEl.appendChild(newChannelContainer);
+          // better if I assigned a class to this div and styled in the css file?
+      newChannelContainer.style.width = "100%";
       newChannelContainer.style.display = "flex";
-      console.log(channel.color);
+      newChannelContainer.style.justifyContent = "space-evenly";
+      newChannelContainer.style.border = "solid 4px black"
       newChannelContainer.style.backgroundColor = "#" + channel.color;
+      channelsEl.appendChild(newChannelContainer);
 
       const newChannelImg = document.createElement("img");
       newChannelImg.src = channel.image;
+      if (channel.image == undefined ) {
+        console.log("image for channel id " + channel.id + " could not load.");
+      }
+      newChannelImg.style.width = "50%";
       newChannelContainer.appendChild(newChannelImg);
 
+      const nameAndaudioContainer = document.createElement("div");
+      nameAndaudioContainer.style.width = "50%";
+      nameAndaudioContainer.style.display = "flex";
+      nameAndaudioContainer.style.flexDirection = "column";
+      nameAndaudioContainer.style.justifyContent = "center";
+      nameAndaudioContainer.style.alignItems = "center";
+      newChannelContainer.appendChild(nameAndaudioContainer);
+
       const newChannelName = document.createElement("div");
+      newChannelName.style.padding = "0 20px";
+     /*  newChannelName.style.width = "100%"; */ // if I want to set the name at the start of the div or something...
       newChannelName.innerHTML = "<h2>" + channel.name + "</h2>";
-      newChannelContainer.appendChild(newChannelName);
-
-    });
-
-    // liveaudio contains: id, statkey, url
-  /*   data.channels.liveaudio.url.forEach( (url) => {
+      nameAndaudioContainer.appendChild(newChannelName);
+       
       const newAudioEl = document.createElement("audio");
-      newAudioEl.src = url;
-      newAudioEl
+      newAudioEl.style.width = "70%"; 
+      newAudioEl.style.padding = "10px";
+      newAudioEl.controls = "true";
+      newAudioEl.type = "audio/mpeg";
+      newAudioEl.src = channel.liveaudio.url; 
+      nameAndaudioContainer.appendChild(newAudioEl);
     });
-  */
+  
   }
 
   getChannels();
